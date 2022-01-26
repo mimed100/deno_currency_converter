c09da1e262e7a66d2af7da89260226f6fadd96b7
mimed100 <michel.medved@gmail.com>
2022-01-26 18:08:30 +0100

    fixer.io does not support the Endpoint to change the base of the currencies.... :(

--- mod.ts
+++ mod.ts
@@ -3,19 +3,20 @@ import {readJson, readJsonSync, writeJson, writeJsonSync} from 'https://deno.lan
 import {API} from './src/api.ts'
 
 // Enter your private api access key as string HERE!
-//const api_key = new API("928d9df316f02c8475d137a189d97112")
-const api_key = new API("YOUR PRIVATE API KEY COMES HERE!")
+const api_key = new API("928d9df316f02c8475d137a189d97112")
+//const api_key = new API("YOUR PRIVATE API KEY COMES HERE!")
 
-console.log("YOUR private API KEY: ", api_key.get_api_key());
-console.log("##############################################");
-console.log("API REQUEST WITH YOUR KEY WILL CREATE A LOCAL JSON FILE!");
+console.log("YOUR private API KEY: \n"+api_key.get_api_key());
+console.log("###########################################################################");
 
 
-//const data = await Request.get(api_key.get_api_key());
-//console.log(data);
-//writeJsonSync('data.json', data)
+const data = await Request.get(api_key.get_api_key());
+console.log(data);
+writeJsonSync('data.json', data)
 
 
 let object:any = readJsonSync("data.json")
-console.log(typeof object)
-console.log(object.rates.AED)
+console.log(object.base);
+
+
+
