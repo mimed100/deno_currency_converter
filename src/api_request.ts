0c28a20ae19d78edc34724162d33e557249bdc8f
mimed100 <michel.medved@gmail.com>
2022-01-27 00:55:20 +0100

    aufsplitten der einzelnen funktionen in einzelne .ts dateien

---
+++ src/api_request.ts
@@ -0,0 +1,6 @@
+//import {Request} from 'https://deno.land/x/request@1.3.2/mod.ts'
+import{api_key} from './api_key.ts'
+import {readJson, readJsonSync, writeJson, writeJsonSync} from 'https://deno.land/x/jsonfile/mod.ts'
+
+//const data = await Request.get(api_key.req_api_key)
+//export const exData = await writeJsonSync("./data.json", data)
