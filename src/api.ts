4bbb6ff3f6ccb6054145f14aa719aac18dc8dd89
mimed100 <michel.medved@gmail.com>
2022-01-26 18:45:44 +0100

    schönere .json file

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export class API {
     api_key:string;
     constructor(access_key: string) {
-        this.api_key = "http://data.fixer.io/api/latest?access_key="+access_key+"&base=USD";
+        this.api_key = "http://data.fixer.io/api/latest?access_key="+access_key;
     }
     get_api_key() {
         return this.api_key
