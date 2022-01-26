10ec2440ee00a30598ec8773a9565b14a4909bb6
mimed100 <michel.medved@gmail.com>
2022-01-26 14:15:12 +0100

    simplify fetch API

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
-async function downloadExchangeData() {
-    const response = await fetch("ENTER THE API_KEY HERE", {
-        method:"GET",
-    });
-    const exchangeData = await response.json();
-    console.log(exchangeData)
-    }
+import { Request } from 'https://deno.land/x/request@1.3.2/mod.ts'
+import { readJson, readJsonSync, writeJson, writeJsonSync} from 'https://deno.land/x/jsonfile/mod.ts'
 
-    downloadExchangeData();
+
+const data = await Request.get('ENTER YOUR API KEY HERE')
+writeJsonSync('.data.json', data)
+
+console.log(typeof data)
+console.log(data)
