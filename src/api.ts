80d344e5cebbf50132c87e6db07de61dfb3ef940
mimed100 <michel.medved@gmail.com>
2022-01-26 22:17:33 +0100

    restructured, problem solves Danke Jan!

--- src/api.ts
+++ src/api.ts
@@ -2,39 +2,27 @@ import {Request} from 'https://deno.land/x/request@1.3.2/mod.ts'
 //import {readJson, readJsonSync, writeJson, writeJsonSync} from 'https://deno.land/x/jsonfile/mod.ts'
 import loadJsonFile from 'https://deno.land/x/load_json_file@v1.0.0/mod.ts'
 
-class API {
-    api_key:string;
-    data:any;
-    constructor(access_key: string) {
-        this.api_key = "http://data.fixer.io/api/latest?access_key="+access_key;
-        this.data = this.get_data()
-        console.log(this.data)
-    }
-    // Suuupiiiii
-    async get_data(){
-        this.data = await loadJsonFile("./data.json");
-        
-        // Klappt
-        console.log(this.data.rates["USD"]);
-        console.log(typeof this.data.rates["USD"]);
-        
-        //const data_new = data.rates;
-        //console.log(data_new);
-    }
-    get_api_key() {
-        return this.api_key
-    }
-    get_convert(quantity:number, curr_from:string, curr_to:string) {
-        console.log(this.data)
-        //var curr_from_value = this.data.rates[curr_from]
-        //var curr_to_value = this.data.rates[curr_to]
-        //return [quantity, curr_from_value, curr_to_value]
-        
-    }
-}
-const api_key = new API("YOUR PRIVATE API KEY COMES HERE!")
 
-console.log(api_key.get_convert(20, "USD", "EUR"))
+class ExchangeData {
+    api_key:string;
+    exData:any;
+
+    constructor(access_key: string) {
+        this.api_key = "http://data.fixer.io/api/latest?access_key=123456789"
+    }
+
+    async get_data(){
+        this.exData = await loadJsonFile("./data.json");
+    }
+
+    get_convert(quantity:number, curr_from:string, curr_to:string) {
+        return this.get_data().then(()=>{
+            return this.exData.rates[curr_to]})
+    }
+
+}
+const data = new ExchangeData("123456789")
+console.log(await data.get_convert(20, "USD", "EUR"))
 
 
 
@@ -43,13 +31,6 @@ console.log(api_key.get_convert(20, "USD", "EUR"))
 
 //const result:any = await readJsonSync("./data.json");
 //console.log(result.rates["EUR"]);
-
-
-
-
-
-
-
 // Enter your private api access key as string HERE!
 //const api_key = new API("")
 //const api_key = new API("YOUR PRIVATE API KEY COMES HERE!")
