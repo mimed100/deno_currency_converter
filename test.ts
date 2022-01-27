a1920ba2d7a896238a02f80cb7f5aa6853f0a007
mimed100 <michel.medved@gmail.com>
2022-01-27 23:30:55 +0100

    alsmost done

--- test.ts
+++
@@ -1,5 +0,0 @@
-import {FiatConverter_fromJSON} from './src/convert_fromJSON.ts'
-
-let FiatConverter = new FiatConverter_fromJSON()
-var x = await FiatConverter.convert(155, "EUR", "USD")
-console.log(x);
