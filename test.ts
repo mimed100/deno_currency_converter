44a7dba8909e34a21d46dcc1097a8b68df82162b
mimed100 <michel.medved@gmail.com>
2022-02-18 22:03:06 +0100

    01

--- test.ts
+++ test.ts
@@ -1,3 +1,8 @@
-import {FiatConverter_fromJSON} from 'https://deno.land/x/currency_converter@v0.9.1/json_mod.ts'
+//import json from 'https://deno.land/x/currency_converter@v0.9.2/data.json' assert { type: 'json' };
+//console.log(json)
+//console.log(local json data)
+
+
+import {FiatConverter_fromJSON} from "https://deno.land/x/currency_converter@v0.9.2/json_mod.ts"
 let FiatConverter = new FiatConverter_fromJSON()
-console.log(await FiatConverter.convert(15,"EUR","USD"))
+console.log(await FiatConverter.convert(15,"EUR","RUB"))
