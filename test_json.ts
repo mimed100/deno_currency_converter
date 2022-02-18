425f4d70a8b00fddcbd3f4d4f4fee02eea2fd46f
mimed100 <michel.medved@gmail.com>
2022-02-18 23:26:21 +0100

    05

--- test_json.ts
+++ test_json.ts
@@ -8,8 +8,7 @@ var result = await FiatConverter.convert(100, "EUR", "USD")
 console.log('Result:', result, " USD")
 */
 
-import json from 'https://deno.land/x/currency_converter@v1.0.0/data.json' assert { type: 'json' };
-console.log(json)
-
-const write = Deno.writeTextFile("./TEST.json", JSON.stringify(json))
-write.then(() => console.log("File written to ./data.json"));
+import {FiatConverter_fromJSON} from 'https://deno.land/x/currency_converter@v1.0.0/json_mod.ts'
+let FiatConverter = new FiatConverter_fromJSON()
+var result = await FiatConverter.convert(15,"EUR","RUB")
+console.log(result)
